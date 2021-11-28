b1ff7443f56ee9e5cbc11962334994e2c5459e2c
August Miller <hello@gusmiller.com>
2021-11-27 19:44:32 -0800

    Comprehensive default values

--- docs/assets/js/lib/constants.js
+++ docs/assets/js/lib/constants.js
@@ -7,7 +7,16 @@ const INITIAL_CIRCLE_DIAMETER = 1;
 const DEFAULT_SETTINGS = {
     map: 'erangel',
     mode: MODES.DEFAULT,
-    bz: [],
+    bz: [
+        { delay: 120,   wait: 300,  move: 300,  shrink: 0.5,    spread: 0.5 },
+        { delay: 0,     wait: 200,  move: 140,  shrink: 0.65,   spread: 0.5 },
+        { delay: 0,     wait: 150,  move: 90,   shrink: 0.5,    spread: 0.5 },
+        { delay: 0,     wait: 120,  move: 60,   shrink: 0.5,    spread: 0.5 },
+        { delay: 0,     wait: 120,  move: 40,   shrink: 0.5,    spread: 0.5 },
+        { delay: 0,     wait: 90,   move: 30,   shrink: 0.5,    spread: 0.5 },
+        { delay: 0,     wait: 90,   move: 30,   shrink: 0.5,    spread: 0.5 },
+        { delay: 180,   wait: 15,   move: 15,   shrink: 0.5,    spread: 0.5 },
+    ],
 };
 
 export {
